58bbaf4f2a0dbcd183cc4f2ccc35f94387c0f70f
Prajwal Veeranna Rayar <183174626+Prajwalrayar@users.noreply.github.com>
2026-02-08 10:01:31 +0530

    Added Employee Api module and updated User Module

--- src/main/java/com/capstone/mapper/UserMapper.java
+++ src/main/java/com/capstone/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.capstone.model.Users;
 import com.capstone.payload.dto.UserDto;
 import com.capstone.payload.response.AuthResponse;
 
+import java.time.LocalDateTime;
+
 public class UserMapper {
 
     public static UserDto toDTO(Users savedUser) {
@@ -18,7 +20,23 @@ public class UserMapper {
         userDto.setUpdatedAt(savedUser.getUpdatedAt());
         userDto.setLastLogin(savedUser.getLastLogin());
         userDto.setPhone(savedUser.getPhone());
+        userDto.setBranchId(savedUser.getBranch()!=null? savedUser.getBranch().getId():null);
+        userDto.setStoreId(savedUser.getStore()!=null? savedUser.getStore().getId():null);
+
 
         return userDto;
+    }
+
+    public static Users toEntity(UserDto userDto) {
+        Users createdUser = new Users();
+        createdUser.setEmail(userDto.getEmail());
+        createdUser.setFullName(userDto.getFullName());
+        createdUser.setRole(userDto.getRole());
+        createdUser.setCreatedAt(LocalDateTime.now());
+        createdUser.setUpdatedAt(LocalDateTime.now());
+        createdUser.setLastLogin(LocalDateTime.now());
+        createdUser.setPhone(userDto.getPhone());
+        createdUser.setPassword(userDto.getPassword());
+        return createdUser;
     }
 }
--- src/main/java/com/capstone/repository/UserRepository.java
+++ src/main/java/com/capstone/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.capstone.repository;
 
+import com.capstone.domain.UserRole;
+import com.capstone.model.Branch;
+import com.capstone.model.Store;
 import com.capstone.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     Users findByEmail(String email);
+    List<Users> findByStore(Store store);
+    List<Users> findByBranchId(Long  branchId);
 }
