494c0e72321843753c82b9c4287d2d4c835a065c
Prajwal Veeranna Rayar <183174626+Prajwalrayar@users.noreply.github.com>
2026-03-07 21:38:45 +0530

    Clean unused file from project root

---
+++ Backend/src/main/java/com/capstone/domain/UserRole.java
@@ -0,0 +1,13 @@
+package com.capstone.domain;
+
+public enum UserRole {
+
+    ROLE_ADMIN,
+    ROLE_STORE_ADMIN,
+    ROLE_STORE_MANAGER,
+    ROLE_BRANCH_MANAGER,
+    ROLE_BRANCH_ADMIN,
+    ROLE_BRANCH_CASHIER,
+    ROLE_CUSTOMER,
+    ROLE_CASHIER
+}
