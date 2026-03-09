68f80b9c3625a9658168eca35c03a933ff0bcf6f
Prajwal Veeranna Rayar <183174626+Prajwalrayar@users.noreply.github.com>
2026-03-09 15:34:41 +0530

    Revert "Clean unused file from project root"

--- src/main/java/com/capstone/repository/OrderItemRepository.java
+++ src/main/java/com/capstone/repository/OrderItemRepository.java
@@ -22,7 +22,7 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 
     @Query("""
-        SELECT c.name, SUM(oi.quantity * oi.price) as totalAmount, SUM(oi.quantity)
+        SELECT c.name, SUM(oi.quantity * oi.price) as totalAmount
         FROM OrderItem oi
         JOIN oi.product p
         JOIN p.category c
